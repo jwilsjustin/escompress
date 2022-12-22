aa9285e214053118ff5ec363458cbdd552b91199
Justin Wilson <justin@moonclerk.com>
2022-12-22 12:55:00 -0500

    use class instance variables instead of class variables

--- test/test_escompress.rb
+++ test/test_escompress.rb
@@ -1,14 +1,15 @@
 require "minitest/autorun"
 require "escompress"
+require "pathname"
 
 class TestEscompress < Minitest::Test
   def test_default_executable_location
-    assert_equal Escompress.esbuild_executable, "/usr/local/bin/esbuild"
+    Escompress.instance_variable_set(:@esbuild_executable, nil)
+    assert_equal Escompress.esbuild_executable, Pathname.new("/usr/local/bin/esbuild")
   end
 
-  # FIXME: add this after moving away from class vars
-  # def test_configurable_executable_location
-  #   Escompress.esbuild_executable = "/Users/justin/Sites/example/node_modules/esbuild/bin/esbuild"
-  #   assert_equal Escompress.esbuild_executable, "/Users/justin/Sites/example/node_modules/esbuild/bin/esbuild"
-  # end
+  def test_configurable_executable_location
+    Escompress.esbuild_executable = Pathname.new("/Users/justin/Sites/example/node_modules/esbuild/bin/esbuild")
+    assert_equal Escompress.esbuild_executable, Pathname.new("/Users/justin/Sites/example/node_modules/esbuild/bin/esbuild")
+  end
 end
