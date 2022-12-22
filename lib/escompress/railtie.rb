379cbfbe1262d6d212f378c359eb26102fbe52a4
Justin Wilson <justin@moonclerk.com>
2022-12-22 12:19:13 -0500

    configure the railtie to automatically look in ./node_modules/

--- lib/escompress/railtie.rb
+++ lib/escompress/railtie.rb
@@ -1,4 +1,7 @@
 module Escompress
   class Railtie < Rails::Railtie
+    initializer "escompress_railtie.configure_rails_initialization" do
+      Escompress.esbuild_executable = Rails.root.join("node_modules/esbuild/bin/esbuild")
+    end
   end
 end
