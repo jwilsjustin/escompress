30492367d9f961065b96ba34a51e397d6ac85519
Justin Wilson <justin@moonclerk.com>
2023-12-21 12:18:05 -0500

    custom css compressor should be text/css not application/css

--- lib/escompress/railtie.rb
+++ lib/escompress/railtie.rb
@@ -1,6 +1,8 @@
+require "rails/railtie"
+
 module Escompress
-  class Railtie < Rails::Railtie
-    initializer "escompress_railtie.configure_rails_initialization" do
+  class Railtie < ::Rails::Railtie
+    initializer "escompress_railtie.configure_rails_initialization", group: :all do
       Escompress.esbuild_executable = Rails.root.join("node_modules/esbuild/bin/esbuild")
 
       # Adapted from https://github.com/ahorek/terser-ruby/blob/46a23ba/lib/terser/railtie.rb
@@ -10,7 +12,7 @@ module Escompress
           # It is expected to return a hash that includes a :data key.
           # https://github.com/rails/sprockets/blob/main/guides/extending_sprockets.md#compressors
           # https://github.com/rails/sprockets/blob/main/guides/extending_sprockets.md#extension-interface
-          env.register_compressor "application/css", :escompress, Escompress::Compressor.new(loader: :css)
+          env.register_compressor "text/css", :escompress, Escompress::Compressor.new(loader: :css)
           env.register_compressor "application/javascript", :escompress, Escompress::Compressor.new(loader: :js)
         end
       end
